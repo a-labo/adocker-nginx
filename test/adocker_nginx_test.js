75adb52d4498d763fbf47c310e1ec8346ee3e797
Taka Okunishi <okunishinishi@gmail.com>
2016-11-16 00:44:43 +0900

    Version incremented to 1.0.1

---
+++ test/adocker_nginx_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for adockerNginx.
+ * Runs with mocha.
+ */
+'use strict'
+
+const adockerNginx = require('../lib/adocker_nginx.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('adocker-nginx', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Adocker nginx', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
