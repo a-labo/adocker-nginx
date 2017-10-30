0d18a8fec43f0d4d0da18b12a30038d13011c071
okunishinishi <okunishinishi@gmail.com>
2017-10-30 10:32:30 +0900

    Upgrade node

--- test/adocker_nginx_test.js
+++ test/adocker_nginx_test.js
@@ -5,25 +5,24 @@
 'use strict'
 
 const adockerNginx = require('../lib/adocker_nginx.js')
-const { equal, ok } = require('assert')
+const {equal, ok} = require('assert')
 const aport = require('aport')
 const asleep = require('asleep')
 const arequest = require('arequest')
-const co = require('co')
 
 describe('adocker-nginx', function () {
   this.timeout(300000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Adocker nginx', () => co(function * () {
-    let port = yield aport()
+  it('Adocker nginx', async () => {
+    let port = await aport()
     let nginx = adockerNginx('adocker-nginx-test-01', {
       template: `${__dirname}/../misc/mocks/nginx.conf.template`,
       staticDir: `${__dirname}/../misc/mocks/mock-public`,
@@ -31,32 +30,32 @@ describe('adocker-nginx', function () {
       localhostAs: '10.0.2.2'
     })
 
-    let { run, remove, logs, stop, isRunning, hasContainer } = nginx.cli()
+    let {run, remove, logs, stop, isRunning, hasContainer} = nginx.cli()
 
-    if (yield isRunning()) {
-      yield remove({ force: true })
+    if (await isRunning()) {
+      await remove({force: true})
     }
 
-    equal(yield isRunning(), false)
-    equal(yield hasContainer(), false)
-    yield run()
+    equal(await isRunning(), false)
+    equal(await hasContainer(), false)
+    await run()
 
-    equal(yield isRunning(), true)
-    equal(yield hasContainer(), true)
+    equal(await isRunning(), true)
+    equal(await hasContainer(), true)
 
-    let { statusCode, body } = yield arequest(`http://localhost:${port}/index.html`)
+    let {statusCode, body} = await arequest(`http://localhost:${port}/index.html`)
     equal(statusCode, 200)
     ok(body)
 
-    yield asleep(100)
+    await asleep(100)
 
-    yield stop()
+    await stop()
 
-    equal(yield isRunning(), false)
-    equal(yield hasContainer(), true)
+    equal(await isRunning(), false)
+    equal(await hasContainer(), true)
 
-    yield remove({ force: true })
-  }))
+    await remove({force: true})
+  })
 })
 
 /* global describe, before, after, it */
